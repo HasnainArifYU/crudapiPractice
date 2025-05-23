0f4bb8402bdef356e31dd29d2d6f5d4fd689724d
Hasnain Chaudhry <harif@pgh.yearup.org>
2025-05-22 20:39:43 -0400

    update model class

--- src/main/java/com/hasnain/crudapiproject/crudapi/repository/UserRepository.java
+++ src/main/java/com/hasnain/crudapiproject/crudapi/repository/UserRepository.java
@@ -1,5 +1,23 @@
 package com.hasnain.crudapiproject.crudapi.repository;
 
-public class UserRepository {
-    
+import com.hasnain.crudapiproject.crudapi.model.User;
+
+// Import Spring Data JPA tools
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * By extending JpaRepository<User, Long>, we get the following methods for free:
+     *
+     * - save(User user)        → Insert or update a user
+     * - findAll()              → Get a list of all users
+     * - findById(Long id)      → Get a user by ID
+     * - deleteById(Long id)    → Delete a user by ID
+     *
+     * You can also define custom queries later if needed, like:
+     * - User findByEmail(String email);
+     */
 }
